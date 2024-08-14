4aa9e5a833e8b7460ff42702f8759875c1e72978
kysshsy <kysshsy@gmail.com>
2024-08-14 21:14:29 +0800

    add wezterm config

---
+++ wezterm/.wezterm.lua
@@ -0,0 +1,27 @@
+local wezterm = require 'wezterm'
+local act = wezterm.action
+local config = {}
+
+config.keys = {
+{
+    key = 't',
+    mods = 'ALT',
+    action = act.SpawnTab 'CurrentPaneDomain',
+},
+{
+    key = 'w',
+    mods = 'ALT',
+    action = wezterm.action.CloseCurrentTab { confirm = true },
+}
+}
+for i = 1, 8 do
+  -- CTRL+ALT + number to activate that tab
+  table.insert(config.keys, {
+    key = tostring(i),
+    mods = 'ALT',
+    action = act.ActivateTab(i - 1),
+  })
+end
+
+
+return config
